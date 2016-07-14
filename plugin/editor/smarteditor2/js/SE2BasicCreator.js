e05c43a2108c982f61333b50327046f934531223
Haneul Cho <stuykr@naver.com>
2016-07-14 20:35:23 +0900

    해 주세요 워딩 수정, 로그인 스킨 수정

--- plugin/editor/smarteditor2/js/SE2BasicCreator.js
+++ plugin/editor/smarteditor2/js/SE2BasicCreator.js
@@ -1,6 +1,6 @@
 function createSEditor2(elIRField, htParams, elSeAppContainer){
 	if(!window.$Jindo){
-		parent.document.body.innerHTML="진도 프레임웍이 필요합니다.<br>\n<a href='http://dev.naver.com/projects/jindo/download'>http://dev.naver.com/projects/jindo/download</a>에서 Jindo 1.5.3 버전의 jindo.min.js를 다운로드 받아 /js 폴더에 복사 해 주세요.\n(아직 Jindo 2 는 지원하지 않습니다.)";
+		parent.document.body.innerHTML="진도 프레임웍이 필요합니다.<br>\n<a href='http://dev.naver.com/projects/jindo/download'>http://dev.naver.com/projects/jindo/download</a>에서 Jindo 1.5.3 버전의 jindo.min.js를 다운로드 받아 /js 폴더에 복사해 주세요.\n(아직 Jindo 2 는 지원하지 않습니다.)";
 		return;
 	}
 
