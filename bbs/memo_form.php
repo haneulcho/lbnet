83236ec4be657140f75021f17059550d7a99ee16
Haneul Cho <stuykr@naver.com>
2016-07-18 07:02:33 +0900

    쪽지 보내기 화면 상대방 아이디 노출 안 되게 get 변수 안 받음

--- bbs/memo_form.php
+++ bbs/memo_form.php
@@ -10,23 +10,34 @@ if ($is_guest)
 
 $content = "";
 // 탈퇴한 회원에게 쪽지 보낼 수 없음
-if ($me_recv_mb_id)
-{
-    // 받는 사람 아이디
-    $mb = get_member($me_recv_mb_id);
-    if (!$mb['mb_id'])
-        alert_close('회원정보가 존재하지 않습니다.\\n\\n탈퇴한 회원일 수 있습니다.');
-
-    // 받는 사람이 정보공개 안할 시 쪽지 보낼 수 없도록
-    // if (!$mb['mb_open'] && $is_admin != 'super')
-    //     alert_close('정보공개를 하지 않았습니다.');
-
+if($me_id) {
     // 4.00.15
-    $row = sql_fetch(" select me_memo from {$g5['memo_table']} where me_id = '{$me_id}' and (me_recv_mb_id = '{$member['mb_id']}' or me_send_mb_id = '{$member['mb_id']}') ");
-    // 원본 쪽지 내용
-    if ($row['me_memo'])
-    {
-        $content = str_replace("\n", "\n> ", get_text($row['me_memo'], 0));
+    $row = sql_fetch(" select * from {$g5['memo_table']} where me_id = '{$me_id}' and me_recv_mb_id = '{$member['mb_id']}' ");
+
+    // 내가 받은 쪽지가 아니면 확인 못하게
+    if(!isset($row) && $is_admin != 'super') {
+      alert_close ('잘못된 접근입니다');
+    } else {
+      // 받는 사람 아이디
+      // 레볼루션에서는 쪽지 발송시 받는 상대방 아이디를 노출하면 안됨
+      // 따라서 get 변수 없이 me_id로 memo 테이블에 쿼리 날려서 me_recv_mb_id를 직접 db에서 받아옴
+      $me_send_mb_id = $row['me_recv_mb_id'];
+      $me_recv_mb_id = $row['me_send_mb_id'];
+
+      $mb = get_member($me_recv_mb_id);
+
+      if (!$mb['mb_id'])
+          alert_close('회원정보가 존재하지 않습니다.\\n\\n탈퇴한 회원일 수 있습니다.');
+
+      // 받는 사람이 정보공개 안할 시 쪽지 보낼 수 없도록
+      // if (!$mb['mb_open'] && $is_admin != 'super')
+      //     alert_close('정보공개를 하지 않았습니다.');
+
+      // 원본 쪽지 내용
+      if ($row['me_memo'])
+      {
+          $content = str_replace("\n", "\n> ", get_text($row['me_memo'], 0));
+      }
     }
 }
 
