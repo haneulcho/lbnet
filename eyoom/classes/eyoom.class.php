90ab610088f18be37f3adee85f84d9a5a186462f
Haneul Cho <stuykr@naver.com>
2018-05-27 19:22:12 +0900

    시간 표시 년도까지 포함하도록 수정

--- eyoom/classes/eyoom.class.php
+++ eyoom/classes/eyoom.class.php
@@ -420,13 +420,13 @@ class eyoom extends qfile
 		if($time_gap < 15) return '방금전';
 		else if($time_gap < 60) return $time_gap.'초전';
 		else if ($time_gap < 3600) return round($time_gap/60).'분전'; // 1시간 경과
-		else if ($time_gap < 21600) { // 6시간 경과 전까지 h시간 m분전
-			$minute = round(($time_gap%3600)/60);
-			return round($time_gap/3600).'시간 '.$minute.'분전';
-		}
-		else if ($wr_day == $today) return '오늘 '.date('H:i',$time);// 글이 쓰여진 날과 오늘 날이 같으면 오늘
-		else if ($wr_day == $yesterday) return '어제 '.date('H:i',$time);// 글이 쓰여진 날과 어제가 같으면 어제
-		else if ($wr_day == $byesterday) return '엊그제 '.date('H:i',$time);// 글이 쓰여진 날과 엊그제가 같으면 엊그제
+		// else if ($time_gap < 10800) { // 3시간 경과 전까지 h시간 m분전
+		// 	$minute = round(($time_gap%3600)/60);
+		// 	return round($time_gap/3600).'시간 '.$minute.'분전';
+		// }
+		// else if ($wr_day == $today) return '오늘 '.date('H:i',$time);// 글이 쓰여진 날과 오늘 날이 같으면 오늘
+		// else if ($wr_day == $yesterday) return '어제 '.date('H:i',$time);// 글이 쓰여진 날과 어제가 같으면 어제
+		// else if ($wr_day == $byesterday) return '엊그제 '.date('H:i',$time);// 글이 쓰여진 날과 엊그제가 같으면 엊그제
 		else return date($format,$time);
 	}
 
