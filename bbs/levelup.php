4c99b9c3d3719dc23de3cd9cc60ddf1acc283e87
Haneul Cho <stuykr@naver.com>
2018-06-18 14:32:49 +0900

    기존회원 자동등업 기능 개발 완료 (매우 중요)

--- bbs/levelup.php
+++ bbs/levelup.php
@@ -2,8 +2,15 @@
 include_once('./_common.php');
 include_once(EYOOM_PATH.'/common.php');
 
+$url = G5_URL;
 if (!$is_member) {
   alert('로그인해 주세요.', G5_BBS_URL.'/login.php?url='.urlencode('../bbs/levelup.php'));
+} else {
+  if ($member['mb_id']) {
+    $mb_id = $member['mb_id'];
+  } else {
+    alert('정상적인 방법으로 접근해 주세요.', $url);
+  }
 }
 
 if (!$is_admin) {
@@ -11,20 +18,37 @@ if (!$is_admin) {
   $time_limit = 3;
   $next_date = date("Y-m-d H:i:s", strtotime(date($member['mb_email_certify'])." +3 hours"));
 
-  if ($member['mb_level'] >= $level_limit || $member['mb_woman'] == 1) {
-    alert('이미 인증된 회원입니다! 환영해요!');
+  if ($member['mb_level'] >= $level_limit && $member['mb_woman'] == 1) {
+    alert('이미 인증된 회원입니다! 환영해요!', $url);
   }
-  if ($member['mb_woman'] == 3 && $member['mb_email_certify'] > date("Y-m-d", G5_SERVER_TIME - (86400 / $time_limit))) {
-    alert('질문에 대한 답이 틀렸습니다! 3시간 후 다시 시도해주세요.\n(재시도 가능일자: '.$next_date.')');
+  
+  if ($member['mb_woman'] == 3 && (G5_SERVER_TIME - strtotime(date($member['mb_email_certify']))) < (3600 * $time_limit)) {
+    alert('질문에 대한 답이 틀렸습니다! 3시간 후 다시 시도해주세요.\n(재시도 가능일자: '.$next_date.')', $url);
   }
-  // $mb_cnt = sql_fetch(" select count(mb_id) as cnt from {$g5['member_table']} where mb_woman=3 and mb_id = '{$member['mb_id']}' ");
-  // if ($mb_cnt['cnt']) {
-  //   alert("기존 회원 등업 신청은 한 번만 할 수 있습니다!");
-  // }
 }
 
-    // sql_query(" insert into {$g5['member_table']} set mb_id = '{$mb_id}', mb_password = '".get_encrypt_string($mb_password)."', mb_datetime = '".G5_TIME_YMDHIS."', mb_ip = '{$_SERVER['REMOTE_ADDR']}', mb_email_certify = '".G5_TIME_YMDHIS."', {$sql_common} ");
+$qna = array(
+  array("ques"=>"로다 사이트 상단 대분류(알림공간 ~ 부가메뉴) 개수는? (숫자로만 작성, ex. 3개 (x), 3(o))", "ans"=>"7"),
+  array("ques"=>"로다 공포 게시판 1번 게시글(단편 - 채팅)의 게시 날짜는? (년월일 숫자로만 작성, ex. 18년 3월 19일(x), 180319 (o))", "ans"=>"130319"),
+  array("ques"=>"로다 알림공간 하위 메뉴와 자유공간 하위 메뉴 개수를 더한 값은? (숫자로만 작성, ex. 11개 (x), 11(o))", "ans"=>"11"),
+  array("ques"=>"로다 창작물 게시판 1번 게시글의 제목은? (띄어쓰기 정확하게 제목 그대로 작성)", "ans"=>"달이 떴다고 전화를 주시다니요"),
+  array("ques"=>"로다 접속 주소는? (http:// 및 끝 / 제외 작성, ex. http://naver.com (x), naver.com (o))", "ans"=>"roda.kr"),
+  array("ques"=>"로다 운영진은 총 몇 명일까? (숫자로만 작성, ex. 3명 (x), 3 (o))", "ans"=>"1"),
+  array("ques"=>"로다 상단 다이어리 메뉴의 첫 번째 하위 메뉴는? (띄어쓰기 없이 작성)", "ans"=>"일기장"),
+  array("ques"=>"로다 광고홍보 - L업소정보에 등록된 업소 개수는? (숫자로만 작성, ex. 15개 (x), 15(o))", "ans"=>"19")
+);
 
+$rand_ques = array_rand($qna, 2);
+$list = array();
+
+for($i=0; $i<count($rand_ques); $i++) {
+  unset($data);
+
+  $data['ques'] = $qna[$rand_ques[$i]]['ques'];
+  $data['ans'] = $qna[$rand_ques[$i]]['ans'];
+  $data['ansNum'] = $rand_ques[$i];
+  $list[$i] = $data;
+}
 
 // 불법접근을 막도록 토큰생성
 $token = md5(uniqid(rand(), true));
@@ -33,7 +57,7 @@ set_session("ss_cert_no",   "");
 set_session("ss_cert_hash", "");
 set_session("ss_cert_type", "");
 
-$g5['title'] = '기존 회원 등업 (~ 2018.06.31 까지)';
+$g5['title'] = '기존 회원 등업 (~ 2018.07.31 까지)';
 include_once('./_head.php');
 
 include_once($levelup_skin_path.'/levelup.skin.php');
