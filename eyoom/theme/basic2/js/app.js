7e79e66c8a46d09b2de1330e1dfe3a08a1cdc63b
Haneul Cho <stuykr@naver.com>
2018-11-05 05:35:24 +0900

    최근 게시물 자수제한, 모바일 메뉴 fix

--- eyoom/theme/basic2/js/app.js
+++ eyoom/theme/basic2/js/app.js
@@ -179,7 +179,7 @@ jQuery(document).ready(function() {
                     $('.lb_m_nav.right .lb_m_nav_cont').addClass('opened');
                 }, 100);
             }
-            $('body').addClass('fixed');
+            $('html, body').addClass('fixed');
         });
         $('.lb_m_nav').find('.btn_navClose').on('click', function (e) {
             e.preventDefault();
@@ -187,7 +187,7 @@ jQuery(document).ready(function() {
             setTimeout(function () {
                 $('.lb_m_nav').removeClass('opened');
             }, 800);
-            $('body').removeClass('fixed');
+            $('html, body').removeClass('fixed');
         });
     }
 });
