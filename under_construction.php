d91d489852d7a06bade581686d1c4f59d9447b00
Haneul Cho <stuykr@naver.com>
2019-01-03 04:54:11 +0900

    공사중 페이지 설정

---
+++ under_construction.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="ko"><head>
+<meta charset="utf-8">
+<meta http-equiv="imagetoolbar" content="no">
+<meta http-equiv="X-UA-Compatible" content="IE=10,chrome=1">
+<meta name="robots" content="noindex, nofollow">
+<title>업무관리</title>
+<link rel="stylesheet" href="http://localhost/css/default.css">
+<link rel="stylesheet" href="/eyoom/theme/basic2/plugins/bootstrap/css/bootstrap.min.css?ver=2019010303" type="text/css" media="screen">
+<link rel="stylesheet" href="/eyoom/theme/basic2/plugins/font-awesome/css/font-awesome.min.css?ver=2019010303" type="text/css" media="screen">
+<link rel="stylesheet" href="/eyoom/theme/basic2/css/style.min.css?ver=2019010303" type="text/css" media="screen">
+
+<body>
+<div class="wrapper" style="padding-top:20px">
+    <div class="basic-body container">
+        <div class="row">
+            <div class="jumbotron jumbotron-fluid text-center">
+                    <h1 class="display-4" style="font-weight:bold;font-size:42px">이사중!</h1>
+                    <p class="lead margin-top-25 margin-bottom-35" style="font-size:18.5px;line-height:1.5">레볼루션이 활성화 될수록 사이트 속도가 느려져 답답했지?
+                    <br>그래서 우리 운영진은 <span style="color:#D60036;font-weight:bold">서버 확장 이전</span>을 결정했어. (짝짝)<br>니니들의 레볼 라이프를 위해 최선을 다할게. 그럼 새해 복 많이 받아. (_ _)
+                    </p>
+                    <div class="alert alert-warning margin-top-30" role="alert">
+                        <h4 class="alert-heading" style="font-weight:bold;font-size:18px">사이트 이용 중단 기간: 1월 3일(목) 08:00 ~ 1월 4일(금) 23:59:59</h4>
+                        <hr>
+                        <h4 class="alert-heading" style="font-weight:bold;font-size:18px">1월 등업 기간: 서버 이전 완료 후 ~ 1월 6일(일) 23:59:59</h4>
+                        <hr>
+                        <p class="lead" style="font-size:14.5px">이용이 중단될 동안 아래 대기실에서 자유롭게 의견 나눌 수 있어.<br>작업에 따라 예정 시간보다 더 빨리 혹은 더 늦게 처리될 수 있으니 해량 바라 :D</p>
+                    </div>
+                    <div id="lv-container" data-id="city" data-uid="MTAyMC80MTk1Mi8xODQ5OA==">
+                        <script type="text/javascript">
+                    (function(d, s) {
+                        var j, e = d.getElementsByTagName(s)[0];
+
+                        if (typeof LivereTower === 'function') { return; }
+
+                        j = d.createElement(s);
+                        j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
+                        j.async = true;
+
+                        e.parentNode.insertBefore(j, e);
+                    })(document, 'script');
+                        </script>
+                    <noscript> 라이브리 댓글 작성을 위해 JavaScript를 활성화 해주세요</noscript>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
