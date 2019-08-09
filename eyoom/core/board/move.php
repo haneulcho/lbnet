416ee33fa8cb28d89fe5c0195d37ae2b27d66447
Haneul Cho <stuykr@naver.com>
2019-08-09 23:22:46 +0900

    복사, 이동 기능 admin 제한 해제

--- eyoom/core/board/move.php
+++ eyoom/core/board/move.php
@@ -47,10 +47,10 @@
 	//$sql = " select * from {$g5['board_table']} a, {$g5['group_table']} b where a.gr_id = b.gr_id and bo_table <> '$bo_table' ";
 	// 원본 게시판을 선택 할 수 있도록 함.
 	$sql = " select * from {$g5['board_table']} a, {$g5['group_table']} b where a.gr_id = b.gr_id ";
-	if ($is_admin == 'group')
-		$sql .= " and b.gr_admin = '{$member['mb_id']}' ";
-	else if ($is_admin == 'board')
-		$sql .= " and a.bo_admin = '{$member['mb_id']}' ";
+	// if ($is_admin == 'group')
+	// 	$sql .= " and b.gr_admin = '{$member['mb_id']}' ";
+	// else if ($is_admin == 'board')
+	// 	$sql .= " and a.bo_admin = '{$member['mb_id']}' ";
 	$sql .= " order by a.gr_id, a.bo_order, a.bo_table ";
 	$result = sql_query($sql);
 	for ($i=0; $row=sql_fetch_array($result); $i++)
